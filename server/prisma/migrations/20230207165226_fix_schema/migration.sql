e272e0928e0cf656d2506ac80e6ceff87df48ec4
33j33 <jai.arora3@student.amity.edu>
2023-02-07 22:53:04 +0530

    fix schema and run migration

---
+++ server/prisma/migrations/20230207165226_fix_schema/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE `book` MODIFY `name` TEXT NOT NULL,
+    MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `rating` MODIFY `review` TEXT NOT NULL;
--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -4,38 +4,39 @@ const prisma = new PrismaClient();
 
 const bookData: Prisma.BookCreateInput[] = [
   {
-    name: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
+    name: "Dune",
+    author: "Frank Herbert",
     description:
-      "A novel about the decadence and excess of the Roaring Twenties",
-    price: 300,
+      "Set in the distant future amidst a sprawling feudal interstellar empire where noble houses, in control of individual planets, owe allegiance to the Padishah Emperor, Dune tells the story of young Paul Atreides",
+    price: 250,
     ratings: {
       create: [
         {
-          review: "Fantastic book, I loved it",
-          rating: 5,
+          review: "Fantastic scifi classic",
+          rating: 4,
         },
         {
-          review: "Not my cup of tea",
-          rating: 2,
+          review: "Epic space adventure",
+          rating: 5,
         },
       ],
     },
   },
   {
-    name: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    description: "A novel about racism and injustice in a small Southern town",
-    price: 200,
+    name: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    description:
+      "The story follows the misadventures of an unwitting human and his alien friend as they travel through space. The narrative is interspersed with comedic and surreal elements and satirical commentary on human life and society.",
+    price: 2000,
     ratings: {
       create: [
         {
-          review: "Powerful and moving",
-          rating: 5,
+          review: "Hilarious and witty",
+          rating: 4,
         },
         {
-          review: "Not very interesting",
-          rating: 2,
+          review: "Out of this world comedy",
+          rating: 5,
         },
       ],
     },
@@ -62,3 +63,43 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+
+// [
+//   {
+//     name: "The Great Gatsby",
+//     author: "F. Scott Fitzgerald",
+//     description:
+//       "A novel about the decadence and excess of the Roaring Twenties",
+//     price: 300,
+//     ratings: {
+//       create: [
+//         {
+//           review: "Fantastic book, I loved it",
+//           rating: 5,
+//         },
+//         {
+//           review: "Not my cup of tea",
+//           rating: 2,
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     name: "To Kill a Mockingbird",
+//     author: "Harper Lee",
+//     description: "A novel about racism and injustice in a small Southern town",
+//     price: 200,
+//     ratings: {
+//       create: [
+//         {
+//           review: "Powerful and moving",
+//           rating: 5,
+//         },
+//         {
+//           review: "Not very interesting",
+//           rating: 2,
+//         },
+//       ],
+//     },
+//   },
+// ];
