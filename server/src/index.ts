b53594fb11d49ad312f307329747437e274323ea
33j33 <jai.arora3@student.amity.edu>
2023-02-08 00:10:36 +0530

    add cors, dotenv, start and build script

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import bookRouter from "./routes/book.routes";
 import ratingRouter from "./routes/rating.route";
 
+dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/books", bookRouter);
 app.use("/ratings", ratingRouter);
 
-const server = app.listen(3000, () =>
-  console.log(`Server ready at: http://localhost:3000`)
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server ready at: http://localhost:${process.env.PORT}`)
 );
